a09843026fecef782f92c692cd3b63007ed3f2f8
sofiaiia <sofia.s@aland.net>
2022-01-27 10:56:21 +0200

    funktionerna i message-server skapade, inget innehåll ännu

--- server/src/message-server.ts
+++ server/src/message-server.ts
@@ -1,5 +1,38 @@
 import * as WebSocket from 'ws';
 
 export abstract class MessageServer<T>{
-    
+    constructor(private readonly wsServer: WebSocket.Server){
+        this.wsServer.on('connection', this.subscribeToMessages);
+        this.wsServer.on('error',this.cleanupDeadClients);
+    }
+
+    protected readonly subscribeToMessages = (ws: WebSocket): void => {
+
+    };
+
+    private readonly cleanupDeadClients = (): void => {
+
+    };
+
+    protected abstract handleMessage(sender: WebSocket, message: T): void;
+
+    protected broadcastExcept(currentClient: WebSocket, message: Readonly<T>) : void{
+
+    }
+
+    protected replyTo(client: WebSocket, message: Readonly<T>): void{
+
+    }
+
+    protected get clients(): Set<WebSocket>{
+
+    }
+
+    private isAlive(client: WebSocket): boolean{
+
+    }
+
+    private isDead(client: WebSocket): boolean {
+        
+    }
 }
