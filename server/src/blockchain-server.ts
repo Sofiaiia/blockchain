4d0cf47a9c035fda8f8a508e843aa371341be1a0
sofiaiia <sofia.s@aland.net>
2022-01-27 10:17:10 +0200

    skapat grundstruktur och main fil

---
+++ server/src/blockchain-server.ts
@@ -0,0 +1,9 @@
+import * as WebSocket from 'ws';
+import { MessageServer } from './message-server';
+import { Message, MessageTypes, UUID } from './messages';
+
+type Replies = Map<WebSocket, Message>;
+
+export class BlockchainServer extends MessageServer<Message>{
+
+}
