21d880575452b7df26a99d563c6df1e8cd654058
sofiaiia <sofia.s@aland.net>
2022-01-27 12:33:27 +0200

    några fuktioner inplementerade och lite kommentarer tillsatta

--- server/src/blockchain-server.ts
+++ server/src/blockchain-server.ts
@@ -2,11 +2,21 @@ import * as WebSocket from 'ws';
 import { MessageServer } from './message-server';
 import { Message, MessageTypes, UUID } from './messages';
 
+//replies from blockchain nodes
 type Replies = Map<WebSocket, Message>;
 
+//class extends message server 
 export class BlockchainServer extends MessageServer<Message>{
 
+    //Collection of client messages waiting for response 
+    private readonly receivedMessagesAwaitingResponse = new Map<UUID, WebSocket>();
+
+    //Used as accumulator for replies from clients 
+    private readonly sentMessagesAwaitingReply = new Map<UUID, Replies>(); 
+
+    //Handler for all message types
     protected handleMessage(sender: WebSocket, message: Message): void {
+        //switch based on message type, enum in message file 
         switch(message.type){
 
         }
@@ -33,10 +43,10 @@ export class BlockchainServer extends MessageServer<Message>{
     }
 
     private selectTheLongestChain(currentlyLongest: Message, current: Message, index: number){
-
+        return index > 0 && current.payload.length > currentlyLongest.payload.length ? current : currentlyLongest;
     }
 
     private get clientIsNotAlone(): boolean{
-        
+        return this.clients.size > 1;
     }
 }
