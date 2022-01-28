2b559ffa4a47177e3b37f42f1363b710328ce6c0
sofiaiia <sofia.s@aland.net>
2022-01-28 15:47:39 +0200

    funktionerna deklarerade men inte implementerade

--- frontend/src/lib/blockchain-node.ts
+++ frontend/src/lib/blockchain-node.ts
@@ -21,5 +21,66 @@ export type WithoutHash<T> = Omit<T, 'hash'>;
 export type NotMinedBlock = Omit<Block, 'hash' | 'nonce'>;
 
 export class BlockchainNode{
-    
+    private _chain: Block[] = [];
+    private _pendingTransactions: Transaction[] = [];
+    private _isMining = false;
+
+    initializeWith(blocks: Block[]): void{
+        this._chain = [...blocks];
+    }
+
+    async initializeWithGenesisBlock(): Promise<void>{
+
+    }
+
+    async mineBlock(block:NotMinedBlock): Promise<Block>{
+
+    }
+
+    async mineBlockWith(transactions: Transaction[]): Promise<Block>{
+
+    }
+
+    get isMining(): boolean{
+
+    }
+
+    get chain(): Block[]{
+
+    }
+
+    get chainIsEmpty(): boolean{
+
+    }
+
+    get latestBlock(): Block{
+
+    }
+
+    get pendingTransactions(): Transaction[]{
+
+    }
+
+    get hasPendingTransactions(): boolean{
+
+    }
+
+    get noPendingTransactions(): boolean{
+
+    }
+
+    addTransaction(transaction: Transaction): void{
+
+    }
+
+    async addBlock(newBlock: Block): Promise<void>{
+
+    }
+
+    private async calculateHash(block: WithoutHash<Block>): Promise<string>{
+
+    }
 }
+    export function randomDelay(maxMilliseconds:number = 100): Promise<void>{
+
+    }
