485882b8cf7b6fd532f6b30a0b1afca06cb89e28
sofiaiia <sofia.s@aland.net>
2022-01-30 13:13:39 +0200

    fortsätter implementera funktioenr med n¨ågon error ännu

--- frontend/src/lib/websocket-controller.ts
+++ frontend/src/lib/websocket-controller.ts
@@ -1,16 +1,17 @@
+import { transform } from '@vue/compiler-core';
 import { Block, Transaction } from './blockchain-node';
 import { uuid } from './cryptography';
 import { Message, MessageTypes, UUID } from './messages';
 
-interface PromiseExecutor<T>{
+interface PromiseExecutor<T> {
     resolve: (value?: T | PromiseLike<T>) => void;
     reject: (reason?: any) => void;
-}
+}  
 
 export class WebsocketController{
     private websocket!: Promise<WebSocket>;
     private messagesCallback!: (messages: Message) => void;
-    private readonly messagesAwitingReply = new Map<UUID, PromiseExecutor<Message>>();
+    private readonly messagesAwaitingReply = new Map<UUID, PromiseExecutor<Message>>();
 
     //URL to server 
     private get url(): string{
@@ -36,22 +37,49 @@ export class WebsocketController{
     }
 
     private readonly onMessageReceived = (event: MessageEvent) => {
+        const message = JSON.parse(event.data) as Message;
 
+        if(this.messagesAwaitingReply.has(message.correlationId)){
+            this.messagesAwaitingReply.get(message.correlationId)!.resolve(message);
+            this.messagesAwaitingReply.delete(message.correlationId);
+        }else{
+            this.messagesCallback(message);
+        }
     }
 
     async send(message: Partial<Message>, awaitForReply: boolean = false): Promise<Message> {
-
+        return new Promise<Message>(async (resolve, reject) => {
+          if (awaitForReply) {
+            this.messagesAwaitingReply.set(message.correlationId!, { resolve, reject });
+          }
+          this.websocket.then(
+            ws => ws.send(JSON.stringify(message)),
+            () => this.messagesAwaitingReply.delete(message.correlationId!)
+          );
+        });
     }
 
     async requestLongestChain(): Promise<Block[]> {
-
+        const reply = await this.send({
+          type: MessageTypes.GetLongestChainRequest,
+          correlationId: uuid()
+        }, true);
+        return reply.payload;
     }
 
     requestNewBlock(transactions: Transaction[]): void {
-
+        this.send({
+          type: MessageTypes.NewBlockRequest,
+          correlationId: uuid(),
+          payload: transactions
+        });
     }
-
+    
     announceNewBlock(block: Block): void {
-
+        this.send({
+          type: MessageTypes.NewBlockAnnouncement,
+          correlationId: uuid(),
+          payload: block
+        });
     }
 }
