24a4e9cceb073dbdf6c640fc8d53ea0fad7c7075
sofiaiia <sofia.s@aland.net>
2022-01-30 11:58:49 +0200

    funktionsdeklarationer i websocket controllern gjord

--- frontend/src/lib/websocket-controller.ts
+++ frontend/src/lib/websocket-controller.ts
@@ -8,5 +8,39 @@ interface PromiseExecutor<T>{
 }
 
 export class WebsocketController{
+    private websocket!: Promise<WebSocket>;
+    private messagesCallback!: (messages: Message) => void;
+    private readonly messagesAwitingReply = new Map<UUID, PromiseExecutor<Message>>();
 
+    private get url(): string{
+
+    }
+
+    connect(messagesCallback: (messages: Message)=> void): Promise<WebSocket>{
+
+    }
+
+    disconnect(){
+
+    }
+
+    private readonly onMessageReceived = (event: MessageEvent) => {
+
+    }
+
+    async send(message: Partial<Message>, awaitForReply: boolean = false): Promise<Message> {
+
+    }
+
+    async requestLongestChain(): Promise<Block[]> {
+
+    }
+
+    requestNewBlock(transactions: Transaction[]): void {
+
+    }
+
+    announceNewBlock(block: Block): void {
+        
+    }
 }
