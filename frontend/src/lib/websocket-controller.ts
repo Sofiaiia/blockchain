429d62c301d1cfd34218f5b62fa19591a29bcf37
sofiaiia <sofia.s@aland.net>
2022-01-30 12:16:01 +0200

    börjat implementerat funktionerna

--- frontend/src/lib/websocket-controller.ts
+++ frontend/src/lib/websocket-controller.ts
@@ -12,16 +12,27 @@ export class WebsocketController{
     private messagesCallback!: (messages: Message) => void;
     private readonly messagesAwitingReply = new Map<UUID, PromiseExecutor<Message>>();
 
+    //URL to server 
     private get url(): string{
-
+        const protocol = window.location.protocol === 'https:' ? 'wws' : 'ws';
+        const hostname = process.env.VUE_APP_WS_PROXY_HOSTNAME || window.location.host; 
+        return `${protocol}://${hostname}`;
     }
 
+    //connect to server 
     connect(messagesCallback: (messages: Message)=> void): Promise<WebSocket>{
-
+        this.messagesCallback = messagesCallback;
+        return this.websocket = new Promise((resolve,reject) => {
+            const ws = new WebSocket(this.url);
+            ws.addEventListener('open', () => resolve(ws));
+            ws.addEventListener('error', err => reject(err));
+            ws.addEventListener('message', this.onMessageReceived);
+        });
     }
 
+    //disconnect from server
     disconnect(){
-
+        this.websocket.then(ws=>ws.close());
     }
 
     private readonly onMessageReceived = (event: MessageEvent) => {
@@ -41,6 +52,6 @@ export class WebsocketController{
     }
 
     announceNewBlock(block: Block): void {
-        
+
     }
 }
